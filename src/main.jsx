6e4f7cf8c8c0ba31a1a02609c2d91943247c46ca
Alan <aalan.lima@live.com>
2023-04-12 13:02:03 -0300

    inicia a arquitetura de pastas usando ITCSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './styles/settings/colors.css'
+import './styles/generic/normalize.css'
+import './styles/elements/base.css'
 import App from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
