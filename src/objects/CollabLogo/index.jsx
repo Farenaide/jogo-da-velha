ff1d013cc3fdb09387ccfa37bbe310538e3364d6
Alan <aalan.lima@live.com>
2023-04-13 13:42:17 -0300

    Criado CollabLogo object

---
+++ src/objects/CollabLogo/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles.css'
+import Logo from '../../assets/logo.svg'
+
+const CollabLogo = ()=>(
+    <img className="collab-logo" src={Logo} alt="Collabcode logotipo" />
+)
+
+
+export default CollabLogo
