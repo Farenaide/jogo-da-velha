6d3801a6cce1435ee4ea0879cd2dc9a27f91885d
Alan <aalan.lima@live.com>
2023-04-13 15:10:40 -0300

    Criado o Menu object

---
+++ src/objects/Menu/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './styles.css'
+
+const menu = ()=>(
+    <a href="#menu" className="menu">
+        <span className="center">Menu</span>
+    </a>
+)
+
+
+export default menu
